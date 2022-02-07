b7f5aae5fbb31f93b5f1f665174b88d939b65163
Tomas Mateo <tomas@factory14.com>
2022-02-07 15:39:21 -0300

    test

--- studio/schemas/fieldTranslation.js
+++ studio/schemas/fieldTranslation.js
@@ -1,18 +1,18 @@
-import { languages, baseLanguage } from './languages'
+import { languages, baseLanguage } from "./languages";
 
 // This is the basis for all fields that should have translations.
 // Its used to fill out more complete field definitions in
 // 'translateDocs' below
 const languageField = {
-  type: 'object',
+  type: "object",
   fieldsets: [
     {
-      title: 'Translations',
-      name: 'translations',
-      options: { collapsible: true }
+      title: "Translations TEST",
+      name: "translations",
+      options: { collapsible: true },
     },
   ],
-}
+};
 
 // We need to figure out which language to map the preview
 // of this document to. Use the 'localizedPreview' object
@@ -20,29 +20,29 @@ const languageField = {
 // support custom previews, only the build in 'title', 'subtitle'
 // and 'media' properties.
 const localizePreview = (preview) => {
-  if (!preview) return null
-  const { select } = preview
-  if (!select) return null
+  if (!preview) return null;
+  const { select } = preview;
+  if (!select) return null;
   return {
     select: {
-      ...(select.title
-        && { title: `${select.title}.${baseLanguage.name}` }),
-      ...(select.subtitle
-        && { subtitle: `${select.subtitle}.${baseLanguage.name}` }),
-      ...(select.media
-        && { media: `${select.media}.${baseLanguage.name}` }),
-    }
-  }
-}
+      ...(select.title && { title: `${select.title}.${baseLanguage.name}` }),
+      ...(select.subtitle && {
+        subtitle: `${select.subtitle}.${baseLanguage.name}`,
+      }),
+      ...(select.media && { media: `${select.media}.${baseLanguage.name}` }),
+    },
+  };
+};
 
 export const translateFields = (docs) => {
   const documents = docs.map((doc) => {
     // Change all the fields to object versions with properties for each
     // language, if either the document has localize: true or individual fields
-    const fields = doc.fields.map(field => {
-      const shouldLocalize = field.type !== 'reference' && (doc.localize || field.localize)
+    const fields = doc.fields.map((field) => {
+      const shouldLocalize =
+        field.type !== "reference" && (doc.localize || field.localize);
       // Use the field defined as-is if its not to be translated
-      if (!shouldLocalize || field.localize === false) return field
+      if (!shouldLocalize || field.localize === false) return field;
 
       return {
         ...languageField,
@@ -52,17 +52,17 @@ export const translateFields = (docs) => {
           title: language.title,
           name: language.name,
           // All other languages except the first one is collapsed by default
-          fieldset: i === 0 ? null : 'translations',
-        }))
-      }
-    })
+          fieldset: i === 0 ? null : "translations",
+        })),
+      };
+    });
 
     return {
       ...doc,
       preview: localizePreview(doc.preview) || doc.preview,
-      fields
-    }
-  })
+      fields,
+    };
+  });
 
-  return documents
-}
+  return documents;
+};
